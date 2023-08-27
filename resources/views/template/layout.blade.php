2df0d363cac6fa8cbcf933f859781f2a9c1a806b
WayanAndika <andikabuana45@gmail.com>
2023-08-27 06:25:55 -0700

    Update CSS

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -8,6 +8,37 @@
   <link rel="stylesheet" href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"/>
   <script src="https://cdn.tailwindcss.com"></script>
   <title> {{ $title }} </title>
+  <style>
+    :root{
+    --primary:#E2C47B;
+    --secondary:#FFFFFF;
+    --ternary:#322F2B;
+}
+
+body{
+    background: linear-gradient(to right, var(--primary), var(--ternary));
+    background-attachment: fixed;
+}
+.home-layout{
+    background-image: url("/images/main-img.png");
+    background-repeat: no-repeat;
+    background-position: bottom right;
+    height: 100vh;
+}
+
+.third-content {
+    background-image: url("/images/tenun.png");
+    background-repeat: no-repeat;
+    height: 100vh;
+    background-size: cover;
+}
+.fourth-content {
+    background-image: url("/images/barong.png");
+    background-repeat: no-repeat;
+    height: 100vh;
+    background-size: cover;
+}
+  </style>
 </head>
 <body>
   @yield('content')
